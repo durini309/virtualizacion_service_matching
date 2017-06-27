12a4af3c7214082e523865c485a53a2cf497212e
jdurini <jdurini@protelecom.com.gt>
2017-06-26 21:09:03 -0600

    Se cambia comentario erroneo

--- routes/matching.js
+++ routes/matching.js
@@ -4,7 +4,7 @@ var router = express.Router();
 const matchingController = require('../controllers').matching_controller;
 
 router.get('/:campaign', function(req, res){  
-    console.log('route advertisers reached');
+    console.log('route /:campaign reached');
     matchingController.getAdvertisers(req, res);
 })
 
